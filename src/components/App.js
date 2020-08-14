2674a440a343551110d36f8dbfd70356d372682c
sajidd-dev <sajid.naseem55541@gmail.com>
2020-08-14 10:29:58 +0500

    Add:data/DIsplay-In-List

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../css/App.css';
 import AddAppointments from './AddAppointments';
 import ListAppointments from './ListAppointments';
 import SeachAppointments from './SearchAppointments';
-function App() {
-  return (
-    <main  className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments  />
-              <ListAppointments  />
-              <SeachAppointments  />
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      myAppointments : []
+    }
+  }
+
+  componentDidMount(){
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(result => {
+      const apts = result.map(item => {
+        return item;
+      })
+      this.setState({
+        myAppointments : apts
+      });
+    });
+  }
+  render() {
+   
+    return (
+      <main  className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+              
+                <AddAppointments  />
+                <ListAppointments appointments={this.state.myAppointments} />
+                <SeachAppointments  />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,8 +1,48 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 
-class ListAppointments extends Component{
-    render(){
-        return <div className="mt-2"> List Appointments</div>;
+class ListAppointments extends Component {
+
+    render() {
+        const listItem = this.props.appointments.map(item => (
+            <div>
+                <div>
+                    {item.petName}
+                </div>
+                <div>
+                    {item.ownerName}
+                </div>
+            </div>
+        ));
+        return (
+            <div className="appointment-list item-list mb-3">
+                {this.props.appointments.map(item => (
+                    <div className="pet-item col media py-3">
+                        <div className="mr-3">
+                            <div className="pet-delete btn btn-sm btn-danger">
+                                X
+                        </div>
+                            <div className="pet-info media-body">
+                                <div className="pet-head d-flex">
+                                    <span className="pet-name">
+                                        {item.PetName}
+                                </span>
+                                    <span className="apt-date ml-auto">
+                                        {item.aptDate}
+                                </span>
+                                </div>
+                                <div className="owner-name">
+                                    <span className="label-item">Owner: </span>
+                                    <span>{item.OwnerName}</span>
+                                </div>
+                                <div className="apt-notes">{item.aptNotes}</div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+
+            </div>
+        );
+
     }
 }
 export default ListAppointments;
